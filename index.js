2c2e3c8baa97719bbd8f111b3795d29f51c4d3c6
Bolat Abdulaziz <Bola_cg56@student.itstep.kz>
2024-03-11 12:33:01 +0300

    changes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+const cors = require('cors')
+
 const express = require("express");
 const router = require("./routes");
 const fileParser = require("express-multipart-file-parser");
@@ -13,6 +15,8 @@ app.use(express.json());
 app.use(fileParser);
 
 app.use(router);
+
+app.use(cors)
 
 const port = process.env.PORT || 8080;
 
